c5c502dc366149c5305b856c4697cb6770833623
origin <yba73mos@gmail.com>
2022-05-20 10:26:43 +0100

    first

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Showb from './showb';
 
 export default class App extends Component {
     state ={
-      
+      count : 0,
          show : false,
     }
   render() {
@@ -15,8 +15,13 @@ export default class App extends Component {
    <button style={{color : 'red',fontWeight: 'bold' ,width : 100, height : 50}} onClick ={()=>this.setState({show:!this.state.show})}>SHOW</button>
 
   {this.state.show ? <Showb /> : null}
+  <h1>Life cycle ={this.state.count}</h1>
       </div>
     )
   }
+  componentDidMount() { setInterval(() => {
+    this.setState({count:this.state.count+1})
+  }, 1000); }
+
 }
 
